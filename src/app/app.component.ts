c67cc341a9a89557b1191af323809087ea96a471
Alex Smeall <alexsmeall@gmail.com>
2026-08-31 14:45:40 -0230

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Player } from './models/players';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'lineup-setter';
+
+  players = signal<Player[]>(
+    ['Stevie', 'John', 'Paul', 'George'].map((name, index) => ({
+      id: index + 1,
+      name,
+    }))
+  );
+
+  ngOnInit() {
+    console.log(this.players());
+  }
 }
