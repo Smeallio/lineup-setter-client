81a2dd608f1da8f19ddf7bee8b60c83823aa26ba
Alex Smeall <alexsmeall@gmail.com>
2026-09-09 15:32:44 -0230

    Push full root changes from previous attempt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from "@angular/core";
 import { AuthService } from "./services/auth.service";
 import { HeaderComponent } from "./components/header/header.component";
+import { RosterComponent } from "./components/roster/roster.component";
 
 @Component({
   selector: "app-root",
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RosterComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
