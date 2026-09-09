020fe280e4aa579dbf14400e4adc4ed8739da69f
Alex Smeall <alexsmeall@gmail.com>
2026-09-09 15:31:58 -0230

    Enable login and setup roster component

---
+++ src/app/components/roster/roster.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-roster',
+  standalone: true,
+  imports: [],
+  templateUrl: './roster.component.html',
+  styleUrl: './roster.component.scss',
+})
+export class RosterComponent {}
